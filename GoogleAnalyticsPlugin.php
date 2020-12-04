03208e1f812dd24acdf7b86a76f877a8b8d4cbff
kalbers <ken.albers@gmail.com>
2020-12-04 15:00:55 -0500

    Update tag to new gtag

--- GoogleAnalyticsPlugin.php
+++ GoogleAnalyticsPlugin.php
@@ -49,16 +49,18 @@ class GoogleAnalyticsPlugin extends Omeka_Plugin_AbstractPlugin
             return;
         }
 
-        $accountId = json_encode($accountId);
+        $accountIdEncoded = json_encode($accountId);
+
+        $url='https://www.googletagmanager.com/gtag/js?id='.$accountId;
+        get_view()->headScript()->setAllowArbitraryAttributes(true);
+        get_view()->headScript()->appendFile($url, null, array('async'=>"async"));
 
         queue_js_string("
-            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-            ga('create', $accountId, 'auto');
-            ga('send', 'pageview');
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+        
+            gtag('config', $accountIdEncoded);
             ");
 
     }
